c19842b08b098e8604833c80b73f4cdd86731f57
basievamaryna <vebas.development@gmail.com>
2020-10-06 21:30:17 +0300

    home work7

---
+++ Front-End55/front 1/lessons6/action_page.php
@@ -0,0 +1,11 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Сообщение благодарности</title>
+</head>
+
+<body>
+<div>Спасибо фориа отправлена, менеджер скоро с Вами свяжется</div>
+</body>
+</html>
